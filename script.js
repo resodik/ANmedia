02491703b01ce106bda5bbe05c46a5b3c2ba05a9
resodik <153916452+resodik@users.noreply.github.com>
2026-05-11 12:31:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const links = document.querySelectorAll('.menu a');
 
+links.forEach(link => {
+
+    link.addEventListener('click', function(e) {
+
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+
+    });
+
+});
