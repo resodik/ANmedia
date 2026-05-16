f4a0fa3e928944589e818b55a7a6b436d0313126
resodik <153916452+resodik@users.noreply.github.com>
2026-05-16 17:19:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,47 @@ links.forEach(link => {
     });
 
 });
+
+
+
+  const slides = document.querySelectorAll(".slide");
+  const nextBtn = document.querySelector(".next");
+  const prevBtn = document.querySelector(".prev");
+
+  let currentSlide = 0;
+
+  function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  nextBtn.addEventListener("click", nextSlide);
+  prevBtn.addEventListener("click", prevSlide);
+
+  setInterval(nextSlide, 5000);
+
+const readMoreButtons = document.querySelectorAll(".read-more-btn");
+
+readMoreButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const parent = button.closest(".mobile-collapse");
+
+        parent.classList.toggle("open");
+
+        if (parent.classList.contains("open")) {
+            button.textContent = "Zwiń";
+        } else {
+            button.textContent = "Czytaj więcej";
+        }
+    });
+});
